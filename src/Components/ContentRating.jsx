e2b03fbb0b70c457f2c0ed5a2d32d5b7dd011e32
Rize-Creator <rizesaguindel27@gmail.com>
2026-06-10 22:04:14 +0800

    first and last commit

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,16 +1,47 @@
-
 import React, { Component } from 'react';
 import './ContentRating.css';
 
 class ContentRating extends Component {
   constructor() {
     super();
+
+    this.state = {
+      likes: 0,
+      dislikes: 0
+    };
   }
+
+  handleLike = () => {
+    this.setState((prevState) => ({
+      likes: prevState.likes + 1
+    }));
+  };
+
+  handleDislike = () => {
+    this.setState((prevState) => ({
+      dislikes: prevState.dislikes + 1
+    }));
+  };
+
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+      <>
+        <div className="content-rating">
+          <p>
+           are you gay?
+          </p>
+
+          <div className="rating-buttons">
+            <button onClick={this.handleLike}>
+              Like ({this.state.likes})
+            </button>
+
+            <button onClick={this.handleDislike}>
+              Dislike ({this.state.dislikes})
+            </button>
+          </div>
+        </div>
+      </>
     );
   }
 }
